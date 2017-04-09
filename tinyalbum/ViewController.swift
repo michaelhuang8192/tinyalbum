a3804fe9fdd4e969bc146b058238dce1425b4247
pk <pk@pks-mac.att.net>
2017-04-08 18:21:21 -0700

    Initial Commit

---
+++ tinyalbum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tinyalbum
+//
+//  Created by pk on 4/8/17.
+//  Copyright © 2017 TinyAppsDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
